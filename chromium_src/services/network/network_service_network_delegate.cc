3a9177c3315f356cb68df5d8b8ed8d72788b70da
Aleksey Khoroshilov <akhoroshilov@brave.com>
2021-08-12 22:16:10 +0700

    Fix network cookies availability in 1PES mode.

--- chromium_src/services/network/network_service_network_delegate.cc
+++ chromium_src/services/network/network_service_network_delegate.cc
@@ -7,11 +7,15 @@
 
 #define OnCanGetCookies OnCanGetCookies_ChromiumImpl
 #define OnCanSetCookie OnCanSetCookie_ChromiumImpl
+#define OnForcePrivacyMode OnForcePrivacyMode_ChromiumImpl
 
 #include "../../../../services/network/network_service_network_delegate.cc"
 
+#undef OnForcePrivacyMode
 #undef OnCanSetCookie
 #undef OnCanGetCookies
+
+using content_settings::EphemeralStorageAwareType;
 
 namespace network {
 
@@ -22,7 +26,8 @@ bool NetworkServiceNetworkDelegate::OnCanGetCookies(
   auto scoped_ephemeral_storage_awareness =
       network_context_->cookie_manager()
           ->cookie_settings()
-          .CreateScopedEphemeralStorageAwareness();
+          .CreateScopedEphemeralStorageAwareness(
+              EphemeralStorageAwareType::kAware);
   return OnCanGetCookies_ChromiumImpl(request, allowed_from_caller);
 }
 
@@ -35,9 +40,24 @@ bool NetworkServiceNetworkDelegate::OnCanSetCookie(
   auto scoped_ephemeral_storage_awareness =
       network_context_->cookie_manager()
           ->cookie_settings()
-          .CreateScopedEphemeralStorageAwareness();
+          .CreateScopedEphemeralStorageAwareness(
+              EphemeralStorageAwareType::kAware);
   return OnCanSetCookie_ChromiumImpl(request, cookie, options,
                                      allowed_from_caller);
+}
+
+bool NetworkServiceNetworkDelegate::OnForcePrivacyMode(
+    const GURL& url,
+    const net::SiteForCookies& site_for_cookies,
+    const absl::optional<url::Origin>& top_frame_origin) const {
+  // Enable ephemeral storage support for the call.
+  auto scoped_ephemeral_storage_awareness =
+      network_context_->cookie_manager()
+          ->cookie_settings()
+          .CreateScopedEphemeralStorageAwareness(
+              EphemeralStorageAwareType::kNotAwareButAllowIn1pEphemeralMode);
+  return OnForcePrivacyMode_ChromiumImpl(url, site_for_cookies,
+                                         top_frame_origin);
 }
 
 }  // namespace network
