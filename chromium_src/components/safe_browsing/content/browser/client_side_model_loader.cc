ec408ea9f653cfe760780f941f88d46cdbd56678
Francois Marier <francois@brave.com>
2021-08-20 18:05:02 -0700

    Prevent the client-side phishing model from being downloaded (fixes brave/brave-browser#16379)

---
+++ chromium_src/components/safe_browsing/content/browser/client_side_model_loader.cc
@@ -0,0 +1,8 @@
+/* Copyright (c) 2021 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#define BRAVE_DISABLE_FETCH return;
+#include "../../../../../../components/safe_browsing/content/browser/client_side_model_loader.cc"  // NOLINT
+#undef BRAVE_DISABLE_FETCH
